55114487e65dc5da9e20f6eaf055c454c099a2ce
Sam Woodman - NOAA <sam.woodman@noaa.gov>
2025-08-08 15:48:30 -0700

    write device-calibrations to Fleet Status

--- scripts/generate-fleet-status.py
+++ scripts/generate-fleet-status.py
@@ -2,6 +2,7 @@ import logging
 
 import gspread
 import sqlalchemy
+import pandas as pd
 
 import esdglider.config as config
 
@@ -20,6 +21,12 @@ from a local computer to access the database
 sqlalchemy engine. It should not be committed to GitHub.
 """
 
+def write_to_sheet(sh, wk_name, df):
+    # wk_name = "Devices-Calibrations"
+    logging.info("Updating the %s worksheet on the %s spreadsheet", wk_name, sh.title)
+    wk = sh.worksheet(wk_name)
+    wk.update([df.columns.values.tolist()] + df.values.tolist())
+
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(module)s:%(asctime)s:%(levelname)s:%(message)s [line %(lineno)d]",
@@ -27,23 +34,44 @@ if __name__ == "__main__":
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
+    gc = gspread.oauth()  # type: ignore
+    sh = gc.open("Fleet Status")
+
     with open("db/glider-db-prod.txt", "r") as f:
         conn_string = f.read()    
         engine = sqlalchemy.create_engine(conn_string)
         with engine.connect() as connection:
-            # Generate deployment table
+            # Generate deployment table, and write to fleet status
             df_depl = config.get_deployment_table(connection)
             x = df_depl.copy(deep=True)
             x = x.drop(["Dates", "Sensors"], axis=1)
-
-            # Write Deployments table to fleet status
-            wk_name = "Deployments-Database"
-            logging.info("Updating the Fleet Status %s sheet", wk_name)
             x = x.fillna("").rename({"Glider_Deployment_ID": "Deployment_ID"})
-            gc = gspread.oauth()  # type: ignore
-            sh = gc.open("Fleet Status")
-            wk = sh.worksheet(wk_name)
-            wk.update([x.columns.values.tolist()] + x.values.tolist())
+            
+            write_to_sheet(sh, "Deployments-Database", x)
+
+            # wk_name = "Deployments-Database"
+            # logging.info("Updating the Fleet Status %s sheet", wk_name)
+            # wk = sh.worksheet(wk_name)
+            # wk.update([x.columns.values.tolist()] + x.values.tolist())
+
+            # Generate device calibrations table, and write to sheet
+            cals = pd.read_sql_table("vDevice_Calibration", connection, "dbo")
+            cals_columns = [
+                'Manufacturer', 'Component', 'Model', 'Serial_Num', 'Calibration_Type',
+                'Calibration_Date', 'Coefficient', 'Calibration_Description', 
+                # 'Device_ID', 'Device_Calibration_ID', 
+                'Calibration_Created_Dt'
+            ]
+            cals = cals[cals_columns].sort_values(["Component", "Serial_Num", "Calibration_Date"])
+            cals["Calibration_Date"] = cals["Calibration_Date"].dt.strftime("%Y-%m-%d")
+            cals["Calibration_Created_Dt"] = cals["Calibration_Created_Dt"].dt.strftime("%Y-%m-%d %H:%M:%S")
+            write_to_sheet(sh, "Devices-Calibrations", cals)
+
+
+            # wk_name = "Devices-Calibrations"
+            # logging.info("Updating the Fleet Status %s sheet", wk_name)
+            # wk = sh.worksheet(wk_name)
+            # wk.update([x.columns.values.tolist()] + x.values.tolist())
 
             # # Update data validation formatting automatically..
             # from gspread.utils import ValidationConditionType
